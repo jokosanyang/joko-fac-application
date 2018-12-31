2ae338ae23648c412c13e19c58364ccd1288e176
Joko Sanyang <43584074+jokosanyang@users.noreply.github.com>
2018-12-31 18:50:10 +0000

    Improved responsiveness, edited title, smoothed bg
    - Fixed buttons jumping
    - Don't show button text if on mobile
    - Added favicon
    - Changed font-size to rem units

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//var images = [];
+//images[0] = "url('images/babyJ.jpg')";
+//images[1] = "url('images/heart2.jpg')";
+//var currentImg = 0;
+//var arraySize = images.length;
+//function changeImg() {
+//                document.querySelector("header").style.backgroundImage = images[currentImg];
+//                currentImg = (currentImg+1)%arraySize;
+//                }
+//
+//setInterval(changeImg, 5000);
+
 function scrollAppear() {
     var text = document.querySelectorAll(".text");
     var scrollPosition = window.innerHeight / 2;
